e4d603e312735183afdcdfa6d0af557bd53c188e
luki <me@lukasparedes.com>
2025-07-12 21:51:02 -0400

    feat: added some testing assets

---
+++ scripts/resources/slot_symbol_resource.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name SlotSymbol
+
+@export var name: String
+@export var type: enum Type {JACKPOT, HIGH, MEDIUM, LOW}
+@export var points: int
+@export var texture: Texture2D
