b1c268f34bb020a2f8f92dbfa783c5b220c36829
luki <lukasethanparedes@gmail.com>
2025-07-17 22:15:26 -0400

    feat: reel spin cell index

--- scripts/reel.gd
+++ scripts/reel.gd
@@ -7,11 +7,11 @@ var reel_is_spinning: bool = false
 
 var sprite2_active: bool = false
 
-
-
 var next_threshold: int = -1134   # when the next reel_sprite should be started, last 3 symbols
 var gone_threshold: int = -1260    # when the reel_sprite is out of sight
 const SCROLL_SPEED = 630
+
+const SPRITE_HEIGHT = 1260
 
 var sprite1_tween: Tween
 var sprite2_tween: Tween
@@ -62,9 +62,32 @@ func sprite2_start() -> void:
 	sprite2_tween.tween_property(reel_sprite2, "position", Vector2(0,-1260), 2.772).from_current()
 	
 func _on_reel_button_1_button_up() -> void:
-	reel_is_spinning = false
+	#print("Sprite1 Position: ", reel_sprite1.position.y, " | Sprite 2 Position: ", reel_sprite2.position.y )
 	if sprite1_tween:
 		sprite1_tween.kill()
 	if sprite2_tween:
 		sprite2_tween.kill()
-		
+	reel_is_spinning = false
+	var sprite1_top = reel_sprite1.position.y
+	var sprite2_top = reel_sprite2.position.y
+	var sprite1_bottom = sprite1_top + SPRITE_HEIGHT
+	var sprite2_bottom = sprite2_top + SPRITE_HEIGHT
+	var visible_sprite
+	if sprite1_top < 126 and sprite1_bottom > 0:
+		print("Sprite 1 visible!")
+		visible_sprite = reel_sprite1
+	elif sprite2_top < 126 and sprite2_bottom > 0:
+		print("Sprite 2 visible!")
+		visible_sprite = reel_sprite2
+	else:
+		print("no visible sprite found. you fucked up somewhere.")
+		return
+	var offset = abs(visible_sprite.position.y)
+	var cell_index = round(offset / 42.0)
+	var snapped_y = - (cell_index * 42)
+	visible_sprite.position.y = snapped_y
+	var top_index = cell_index
+	var mid_index = cell_index + 1
+	var bot_index = cell_index + 2
+	var reel_symbols = [top_index, mid_index, bot_index]
+	print(self.name, ": ", reel_symbols)
